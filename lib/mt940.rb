85960a3a54a1f3c072e57c917f22fd926dc0f168
Daniel Hahn <dha@betterplace.org>
2012-04-20 10:56:32 +0200

    Removed Thies' favourite quote marks due to encoding problems

--- lib/mt940.rb
+++ lib/mt940.rb
@@ -221,7 +221,7 @@ class MT940
           else
             @not_implemented_fields ||= []
             @not_implemented_fields << [code, content]
-            $stderr << "code not implemented: code:#{code} content:»#{content}«\n" if $DEBUG
+            $stderr << "code not implemented: code:#{code} content: \"#{content}\"\n" if $DEBUG
           end
         end
       end
