7ad360deabf0283093c073521d9ab8d1428e40cb
Daniel Hahn <daniel.hahn@betterplace.org>
2025-05-21 13:11:15 +0200

    Add opening/closing balances and signature
    Signature is for duplicate detection, and based on the opening and
    closing balance lines.

--- lib/mt940/customer_statement_message.rb
+++ lib/mt940/customer_statement_message.rb
@@ -3,11 +3,10 @@
 # the data easier
 class MT940
   class CustomerStatementMessage
-
-    attr_reader :account, :statement_lines
+    attr_reader :account, :statement_lines, :opening_balance, :closing_balance
 
     def self.parse_file(file)
-      self.parse(File.read(file))
+      parse(File.read(file))
     end
 
     def self.parse(data)
@@ -16,12 +15,15 @@ class MT940
     end
 
     def initialize(lines)
-      @account = lines.find { |line| line.class == MT940::AccountIdentification }
+      @account = select_by_type(lines, MT940::AccountIdentification)
+      @opening_balance = select_by_type(lines, MT940::AccountBalance)
+      @closing_balance = select_by_type(lines, MT940::ClosingBalance)
       @statement_lines = []
       lines.each_with_index do |line, i|
         next unless line.class == MT940::StatementLine
-        ensure_is_info_line!(lines[i+1])
-        @statement_lines << StatementLineBundle.new(lines[i], lines[i+1])
+
+        ensure_is_info_line!(lines[i + 1])
+        @statement_lines << StatementLineBundle.new(lines[i], lines[i + 1])
       end
     end
 
@@ -33,36 +35,45 @@ class MT940
       @account.account_number
     end
 
+    def signature
+      Digest::SHA256.hexdigest(opening_balance.content.to_s + closing_balance.content.to_s)
+    end
+
     private
 
+    def select_by_type(lines, line_klass)
+      lines.select { |line| line.instance_of?(line_klass) }.first
+    end
+
     def ensure_is_info_line!(line)
-      unless line.is_a? MT940::StatementLineInformation
-        raise Errors::UnexpectedStructureError,
-          "Unexpected Structure; expected StatementLineInformation, "\
-          "but was #{line.class}"
-      end
+      return if line.is_a? MT940::StatementLineInformation
+
+      raise Errors::UnexpectedStructureError,
+            'Unexpected Structure; expected StatementLineInformation, ' \
+            "but was #{line.class}"
     end
   end
 
   class StatementLineBundle
     METHOD_MAP = {
-      :amount                   => :line,
-      :funds_code               => :line,
-      :value_date               => :line,
-      :entry_date               => :line,
-      :account_holder           => :info,
-      :details                  => :info,
-      :account_number           => :info,
-      :bank_code                => :info,
-      :code                     => :info,
-      :transaction_description  => :info,
+      amount: :line,
+      funds_code: :line,
+      value_date: :line,
+      entry_date: :line,
+      account_holder: :info,
+      details: :info,
+      account_number: :info,
+      bank_code: :info,
+      code: :info,
+      transaction_description: :info
     }
 
     def initialize(statement_line, statement_line_info)
-      @line, @info = statement_line, statement_line_info
+      @line = statement_line
+      @info = statement_line_info
     end
 
-    def method_missing(method, *args, &block)
+    def method_missing(method, *args, &)
       super unless METHOD_MAP.has_key?(method)
       object = instance_variable_get("@#{METHOD_MAP[method.to_sym]}")
       object.send(method)
